63be5f20df46f710eed8d2ff86218ff67dc0b5e6
Graham <gsysko@gmail.com>
2019-11-15 14:33:00 +0000

    Add prefs placeholder

--- src/prefs.js
+++ src/prefs.js
@@ -2,5 +2,6 @@ import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
 export default function() {
+  //TODO Placeholder for a preferences panel...
   sketch.UI.message("It's alive 🙌")
 }
